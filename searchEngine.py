6b04f8414a6e7a1876f8fc1afe359d00a34539be
z5098151 <z5098151@unsw.edu.au>
2018-03-29 21:01:59 +1100

    Added paging and upload function

--- searchEngine.py
+++ searchEngine.py
@@ -2,9 +2,10 @@
 
 import os, re, collections, datetime, operator
 from flask import Flask, request, render_template, redirect, url_for, make_response, session
-
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_PATH'] = 4000
 
 @app.route('/')
 def hello_world():
@@ -14,48 +15,69 @@ def hello_world():
 def home():
 	return render_template("home.html")
 
-@app.route('/all', methods=['GET','POST'])
-def show_all():
-	articles = get_all_aritcles()
-	return render_template("all.html", articles = articles)
+@app.route('/all/p<page>', methods=['GET','POST'])
+def show_all(page):
+	page = int(page)
+	total = int(len(os.listdir("templates/NSW/")))
+	articles = get_all_aritcles(page, total)
+	
+	return render_template("all.html", articles=articles, page=page, total=total)
 
 @app.route('/showing/<link>', methods=['GET','POST'])
 def showing_article(link):
-	# html = "NSW/" + link
-	os.system("cp templates/NSW/" + link + " static/" + link)
-	return render_template("showing.html", link = link)
-	
+
+    os.system("rm static/*.html")
+    os.system("cp templates/NSW/" + link + " static/" + link)
+    return render_template("showing.html", link = link)
 	
 @app.route('/upload', methods=['GET','POST'])
 def upload():
 	message = None
 	if request.method == "POST":
 		button = request.form.get("btn")
+		
 		if button == "upload_article":
 			input = request.form["upload_new"]
 			if re.match("\w",input):
 				message = "Uploaded successfully!"
+		elif button == "upload_file":
+			f = request.files['file']
+			file_name = secure_filename(f.filename)
+			f.save(file_name)
+			os.system("mv " + file_name + " uploads/" + file_name)
+			message = "Uploaded successfully!" 
+					
 	return render_template("upload.html", message=message)
 	
 @app.route('/search/<field>', methods=['GET','POST'])
 def search(field):
 	return render_template("home.html")
 
-def get_all_aritcles():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+def get_all_aritcles(page, total):
+
 	list = os.listdir("templates/NSW/")
 	articles = collections.OrderedDict()
+	count = 1
+    
 	for i in sorted(list):
-		with open(os.path.join( "templates/NSW/", i )) as f:
-			for line in f:
-				if re.match("^<title>",line):
-					line = re.sub("^<title>","",line)
-					line = re.sub("<\/title>$","",line)
-					line = re.sub("\n$","",line)
-					# print(line+"\n")
-					articles[i] = line
-					break
+		if count > (page-1) * 100 and count <= page * 100:
+			with open(os.path.join( "templates/NSW/", i )) as f:
+				for line in f:
+					if re.match("^<title>",line):
+						line = re.sub("^<title>","",line)
+						line = re.sub("<\/title>$","",line)
+						line = re.sub("\n$","",line)
+						# print(line+"\n")
+						articles[i] = line
+						break
+		count += 1
+		
 	return articles
 	
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(debug=True, port=1234)
+    app.run(debug=True, port=1345)
