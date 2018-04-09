139db9d40b68ab796d51670711b60e397b0a5498
Hendri Chua <hc@hc.local>
2018-04-09 15:00:26 +1000

    fix folder placement

--- README.md
+++ README.md
@@ -21,3 +21,15 @@ Important:
 
 * Write access to master branch is only scrum master for editing README.md and add some files
 * To merge to master branch, 2 approvals is required. Will increase to 3 or 4 if necessary.
+
+
+
+additional info:
+1. First please copy all the 29907 html documents to the ‘/Legal Case Search App/Legal Case Search App/static/NSW’ folder. There are several existed html documents in the folder but can be ignored. And after this, pycharm may take 20 mins to index the documents.
+
+2. Use pycharm to import the project, need to create a virtual environment or use an existed Virtual Env. There is a file named “requirements.txt” under “/Legal Case Search App” folder. In both situations, please activate the virtual environment, and in the command line, type “pip3 install -r requirements.txt” (the path of requirements.txt may need to be changed) to install required python packages.
+
+3. Because for the sake of reducing uploaded size, all data and vectors are not included, so to use the app, the fresh start is needed. For fresh start, please run the init.py under ‘/Legal Case Search App’, but may require 20-30 mins.
+
+4. After init.py has finished running, can start the server by run the “/Legal Case Search App/searchEngine.py” to start using the app.
+
--- searchEngine.py
+++ searchEngine.py
@@ -1,83 +1,144 @@
 #!/usr/bin/python3
 
 import os, re, collections, datetime, operator
+#change working dir
+here = os.path.dirname(__file__)
+#print(os.getcwd())
+os.chdir(here)
+#print(os.getcwd())
 from flask import Flask, request, render_template, redirect, url_for, make_response, session
 from werkzeug import secure_filename
+import searchEngine_backend as se
+
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_PATH'] = 4000
 
+
+
+
+global current_result
 @app.route('/')
 def hello_world():
     return redirect(url_for('home'))
-	
-@app.route('/home', methods=['GET','POST'])
+
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
-@app.route('/all/p<page>', methods=['GET','POST'])
+
+@app.route('/all/p<page>', methods=['GET', 'POST'])
 def show_all(page):
-	page = int(page)
-	total = int(len(os.listdir("templates/NSW/")))
-	articles = get_all_aritcles(page, total)
-	
-	return render_template("all.html", articles=articles, page=page, total=total)
+    page = int(page)
+    total = int(len(os.listdir("static/NSW/")))
+    #print(total)
+    #print(page)
+    articles = get_all_aritcles(page, total)
 
-@app.route('/showing/<link>', methods=['GET','POST'])
+    return render_template("all.html", articles=articles, page=page, total=total)
+
+
+@app.route('/showing/<link>', methods=['GET', 'POST'])
 def showing_article(link):
-
     os.system("rm static/*.html")
-    os.system("cp templates/NSW/" + link + " static/" + link)
-    return render_template("showing.html", link = link)
-	
-@app.route('/upload', methods=['GET','POST'])
+    os.system("cp static/NSW/" + link + " static/" + link)
+    return render_template("showing.html", link=link)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-	message = None
-	if request.method == "POST":
-		button = request.form.get("btn")
-		
-		if button == "upload_article":
-			input = request.form["upload_new"]
-			if re.match("\w",input):
-				message = "Uploaded successfully!"
-		elif button == "upload_file":
-			f = request.files['file']
-			file_name = secure_filename(f.filename)
-			f.save(file_name)
-			os.system("mv " + file_name + " uploads/" + file_name)
-			message = "Uploaded successfully!" 
-					
-	return render_template("upload.html", message=message)
-	
-@app.route('/search/<field>', methods=['GET','POST'])
+    global current_result
+    message = None
+    if request.method == "POST":
+        button = request.form.get("btn")
+
+        if button == "upload_article":
+            input = request.form["upload_new"]
+            if re.match("\w", input):
+                message = "Uploaded successfully!"
+            #print(input)
+            #print(type(input))
+            #start to transform input text and compute similarities.
+
+            #print("parsed: {}".format(input_parsed))
+            #print(type(input_parsed))
+            # input_parsed = se.hp.parse_input_text(input)
+            # input_vector = se.vo.transform_text_not_first_time(se.vectorizer,[input_parsed])
+            # input_reduced = se.vo.dim_reduction_not_first(input_vector, se.reducer,"static/reduced_vector")
+            # distances, indices, similarity = se.sm.simi_matching(input_reduced,se.reduced_vectors,200)
+            # current_result = se.sm.combine_fname_sim(se.all_filenames, similarity, indices)
+            current_result = se.find_similarity(input, input_savepath = "static/reduced_vector", result_size = 200)
+            #print(current_result[:,[0,2,1]])
+            #print(input_parsed)
+            return redirect(url_for('show_search_result',page = 1))
+        elif button == "upload_file":
+            f = request.files['file']
+
+            print(f)
+            file_name = secure_filename(f.filename)
+            print(file_name)
+            save_path = "static/uploaded_files/{}".format(file_name)
+            f.save(save_path)
+            input = ""
+            with open(save_path) as file:
+                input = file.read()
+                print(input)
+            current_result = se.find_similarity(input, input_savepath="static/uploaded_file_reduced_vector", result_size=200)
+            #os.system("mv " + file_name + " uploads/" + file_name)
+            message = "Uploaded successfully!"
+            return redirect(url_for('show_search_result',page = 1))
+
+    return render_template("upload.html", message=message)
+
+
+@app.route('/search_result/p<page>', methods=['GET','POST'])
+def show_search_result(page):
+    global current_result
+    page = int(page)
+    #total = int(len(os.listdir("static/NSW/")))
+    total = len(current_result)
+    #articles = get_all_aritcles(page, total)
+    articles = current_result[((page - 1) * 100):page * 100,[0,1,2]]
+    #print(articles)
+    return render_template("search_results.html", articles=articles, page=page, total=total)
+
+
+@app.route('/search/<field>', methods=['GET', 'POST'])
 def search(field):
-	return render_template("home.html")
+    return render_template("home.html")
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
-def get_all_aritcles(page, total):
 
-	list = os.listdir("templates/NSW/")
-	articles = collections.OrderedDict()
-	count = 1
-    
-	for i in sorted(list):
-		if count > (page-1) * 100 and count <= page * 100:
-			with open(os.path.join( "templates/NSW/", i )) as f:
-				for line in f:
-					if re.match("^<title>",line):
-						line = re.sub("^<title>","",line)
-						line = re.sub("<\/title>$","",line)
-						line = re.sub("\n$","",line)
-						# print(line+"\n")
-						articles[i] = line
-						break
-		count += 1
-		
-	return articles
-	
+
+def get_all_aritcles(page, total):
+    list = os.listdir("static/NSW/")
+    #print(list)
+    articles = collections.OrderedDict()
+    count = 1
+
+    for i in sorted(list):
+        if count > (page - 1) * 100 and count <= page * 100:
+            with open(os.path.join("static/NSW/", i)) as f:
+                title = se.hp.extract_html_title(f)
+                articles[i] = title
+                #for line in f:
+
+                    # if re.match("^<title>", line):
+                    #     line = re.sub("^<title>", "", line)
+                    #     line = re.sub("<\/title>$", "", line)
+                    #     line = re.sub("\n$", "", line)
+                    #     # print(line+"\n")
+                    #     articles[i] = line
+                    #     break
+        count += 1
+    #print(articles)
+    return articles
+
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(debug=True, port=1345)
